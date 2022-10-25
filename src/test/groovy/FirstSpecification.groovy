948761a8a17cb6d925c8333eca5ff10d8f26b354
Pedro Januário <up202108768@edu.fe.up.pt>
2022-10-25 10:05:05 +0100

    FirstSpecification finished

--- src/test/groovy/FirstSpecification.groovy
+++ src/test/groovy/FirstSpecification.groovy
@@ -4,45 +4,55 @@ import spock.lang.Specification
 class FirstSpecification extends Specification {
     def "one plus one should equal two"() {
         expect:
-            1 + 1 == 2
+        1 + 1 == 2
     }
 
     def "two plus two should equal four"() {
         given:
-            int left = 2
-            int right = 2
+        int left = 2
+        int right = 2
         when:
-            int result = left + right
+        int result = left + right
         then:
-            result == 4
+        result == 4
     }
 
     def "Should be able to remove from list"() {
         given:
-            def list = [1, 2, 3, 4]
+        def list = [1, 2, 3, 4]
         when:
-            list.remove(0)
+        list.remove(0)
         then:
-            list == [1, 3, 4]
+        list == [1, 3, 4]
     }
 
     def "Should get an index out of bounds when removing a non-existing item"() {
         given:
-            def list = [1, 2, 3, 4]
+        def list = [1, 2, 3, 4]
         when:
-            list.remove(20)
+        list.remove(20)
         then:
-            thrown(IndexOutOfBoundsException)
-            list.size() == 4
+        thrown(IndexOutOfBoundsException)
+        list.size() == 4
     }
 
     def "numbers to the power of two"(int a, int b, int c) {
         expect:
-            Math.pow(a,b) == c
+        Math.pow(a, b) == c
         where:
-            a | b | c
-            1 | 2 | 1
-            2 | 2 | 4
-            3 | 2 | 9
+        a | b | c
+        1 | 2 | 1
+        2 | 2 | 4
+        3 | 2 | 9
+    }
+
+    def "distinct_bug_8726"() {
+        given:
+        def deduplicator = Mock(GenericListDeduplicator)
+        deduplicator.deduplicate(_) >> Arrays.asList(1, 2, 4)
+        when:
+        def result = deduplicator.deduplicate(Arrays.asList(1, 2, 4, 2))
+        then:
+        result == Arrays.asList(1, 2, 4)
     }
 }
