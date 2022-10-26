7665c67b0795cde12087a835b04e091fe1e30986
Pedro Januário <up202108768@edu.fe.up.pt>
2022-10-26 17:35:55 +0100

    Lab finished.

--- src/test/groovy/ListAggregatorTest.groovy
+++ src/test/groovy/ListAggregatorTest.groovy
@@ -1,3 +1,4 @@
+import com.aor.numbers.GenericListDeduplicator
 import com.aor.numbers.ListAggregator
 import spock.lang.Specification
 
@@ -30,5 +31,29 @@ class ListAggregatorTest extends Specification {
             int min = aggregator.min(list)
         then:
             min == 1
+    }
+
+    def "The number of distinct elements should be four"() {
+        given:
+            def list = [1, 2, 4, 2, 5]
+            ListAggregator aggregator = new ListAggregator()
+            def deduplicator = Mock(GenericListDeduplicator)
+            deduplicator.deduplicate(_) >> Arrays.asList(1, 2, 4, 5)
+        when:
+            def result = aggregator.distinct(list)
+        then:
+            result == 4
+    }
+
+    def "Distinct bug 8726"() {
+        given:
+            def list = [1, 2, 4, 2]
+            ListAggregator aggregator = new ListAggregator()
+            def deduplicator = Mock(GenericListDeduplicator)
+            deduplicator.deduplicate(_) >> Arrays.asList(1, 2, 4)
+        when:
+            def result = aggregator.distinct(list)
+        then:
+            result == 4
     }
 }
--- src/test/groovy/ListDeduplicatorTest.groovy
+++ src/test/groovy/ListDeduplicatorTest.groovy
@@ -1,3 +1,4 @@
+import com.aor.numbers.GenericListSorter
 import com.aor.numbers.ListDeduplicator
 import spock.lang.Specification
 
@@ -6,6 +7,19 @@ class ListDeduplicatorTest extends Specification {
         given:
             def list = [1, 2, 4, 2, 5]
             def expected = [1, 2, 4, 5]
+            ListDeduplicator deduplicator = new ListDeduplicator()
+        when:
+            def distinct = deduplicator.deduplicate(list)
+        then:
+            expected == distinct
+    }
+
+    def "Deduplicator Bug 8726"() {
+        given:
+            def list = [1, 2, 4, 2, 5]
+            def expected = [1, 2, 4, 5]
+            def sorter = Mock(GenericListSorter)
+            sorter.sort(_) >> Arrays.asList(1, 2, 2, 4, 5)
             ListDeduplicator deduplicator = new ListDeduplicator()
         when:
             def distinct = deduplicator.deduplicate(list)
